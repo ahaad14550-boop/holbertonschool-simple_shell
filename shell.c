70d283f96b13ccaab0083fe8985a0127c037374e
ahaad14550-boop <ahaad.14550@gmail.com>
2026-05-12 10:58:06 +0000

    g

--- shell.c
+++ shell.c
@@ -6,61 +6,57 @@
 #include <string.h>
 
 /**
- * main - UNIX command line interpreter (Simple Shell 0.1)
- * @ac: argument count
- * @av: argument vector
- * @env: environment variables
- * Return: 0 on success
+ * main - Simple Shell 0.1 with robust space handling
+ * @ac: arg count
+ * @av: arg vector
+ * @env: environment
+ * Return: 0
  */
 int main(int ac, char **av, char **env)
 {
 	char *line = NULL;
 	size_t len = 0;
-	ssize_t read;
-	pid_t child_pid;
+	ssize_t nread;
 	int status;
+	pid_t child_pid;
+	char *token;
 	char *argv[2];
 
 	(void)ac;
-
 	while (1)
 	{
-		/* 1. Display Prompt */
 		if (isatty(STDIN_FILENO))
 			write(STDOUT_FILENO, "#cisfun$ ", 9);
 
-		/* 2. Read Input */
-		read = getline(&line, &len, stdin);
-
-		/* 3. Handle EOF (Ctrl+D) */
-		if (read == -1)
+		nread = getline(&line, &len, stdin);
+		if (nread == -1)
 		{
 			if (isatty(STDIN_FILENO))
 				write(STDOUT_FILENO, "\n", 1);
-			free(line);
-			exit(EXIT_SUCCESS);
+			break;
 		}
 
-		/* Remove newline character */
-		if (line[read - 1] == '\n')
-			line[read - 1] = '\0';
+		
+		token = strtok(line, " \n\t\r");
+		
+		
+		if (token == NULL)
+			continue;
 
-		/* 4. Fork and Execute */
-		argv[0] = line;
+		argv[0] = token;
 		argv[1] = NULL;
 
 		child_pid = fork();
 		if (child_pid == -1)
 		{
-			perror("Error");
+			perror("fork");
 			continue;
 		}
-
 		if (child_pid == 0)
 		{
-			/* Execute command */
 			if (execve(argv[0], argv, env) == -1)
 			{
+				
 				perror(av[0]);
 				free(line);
 				exit(EXIT_FAILURE);
@@ -68,11 +64,9 @@ int main(int ac, char **av, char **env)
 		}
 		else
 		{
-			/* Parent waits for child */
 			wait(&status);
 		}
 	}
-
 	free(line);
 	return (0);
 }
