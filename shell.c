8fc07d224e04f5411858104bce12ebf2ddff8edc
root <ahaad.14550@gmail.com>
2026-05-12 11:49:48 +0000

    shell.c0.3

--- shell.c
+++ shell.c
@@ -9,28 +9,54 @@
 extern char **environ;
 
 /**
- * find_path - looks for a command in the PATH directories
- * @cmd: command to find
- * Return: full path of the command, or NULL if not found
+ * _get_path - gets the PATH environment variable manually.
+ *
+ * Return: pointer to the PATH value, or NULL if not found.
+ */
+char *_get_path(void)
+{
+	int i = 0;
+
+	while (environ[i])
+	{
+		if (strncmp(environ[i], "PATH=", 5) == 0)
+			return (environ[i] + 5);
+		i++;
+	}
+	return (NULL);
+}
+
+/**
+ * find_path - looks for a command in the PATH directories.
+ * @cmd: command to find.
+ *
+ * Return: full path of the command, or NULL if not found.
  */
 char *find_path(char *cmd)
 {
-	char *path = getenv("PATH");
+	char *path = _get_path();
 	char *path_copy, *token, *full_path;
 	struct stat st;
 
-	if (!path)
+	if (!cmd)
 		return (NULL);
-
-	
 	if (stat(cmd, &st) == 0)
 		return (strdup(cmd));
+	if (!path || path[0] == '\0')
+		return (NULL);
 
 	path_copy = strdup(path);
+	if (!path_copy)
+		return (NULL);
 	token = strtok(path_copy, ":");
 	while (token)
 	{
 		full_path = malloc(strlen(token) + strlen(cmd) + 2);
+		if (!full_path)
+		{
+			free(path_copy);
+			return (NULL);
+		}
 		sprintf(full_path, "%s/%s", token, cmd);
 		if (stat(full_path, &st) == 0)
 		{
@@ -45,11 +71,12 @@ char *find_path(char *cmd)
 }
 
 /**
- * main - Simple shell 0.3 with PATH handling
- * @ac: arg count
- * @av: arg vector
- * @env: environment
- * Return: 0 on success
+ * main - simple shell 0.3 compliant with Betty style.
+ * @ac: argument count.
+ * @av: argument vector.
+ * @env: environment variables.
+ *
+ * Return: Always 0 on success.
  */
 int main(int ac, char **av, char **env)
 {
@@ -59,12 +86,12 @@ int main(int ac, char **av, char **env)
 	int status, i;
 	pid_t child_pid;
 	char *argv[1024];
-	(void)ac;
 
+	(void)ac;
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, ":) ", 3);
+			write(STDOUT_FILENO, "#cisfun$ ", 9);
 		nread = getline(&line, &len, stdin);
 		if (nread == -1)
 		{
@@ -76,10 +103,8 @@ int main(int ac, char **av, char **env)
 		argv[i] = strtok(line, " \n\t\r");
 		while (argv[i])
 			argv[++i] = strtok(NULL, " \n\t\r");
-
 		if (!argv[0])
 			continue;
-
 		full_path = find_path(argv[0]);
 		if (full_path)
 		{
